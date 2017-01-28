20bfaa004dba32113feffbb368a4ca276fb060f3
vladich <vladichnaprimer@gmail.com>
2017-01-28 22:44:28 +0200

    rename app, register feature turn off

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+    devise_for :users
     resources :projects
     resources "contacts", only: [:new, :create]
     get 'welcome/index'
     root 'welcome#index'
 
-    # get '*path' => redirect('/')
+    get '*path' => redirect('/')
 end
