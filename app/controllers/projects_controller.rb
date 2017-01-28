13847431b8cb92bdf0035012918ff12722ce13f9
vladich <vladichnaprimer@gmail.com>
2017-01-28 22:35:54 +0200

    add devise model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
 
   before_action :find_project, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @project = Project.all.order('created_at ASC')
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
     resources :projects
     resources "contacts", only: [:new, :create]
     get 'welcome/index'
